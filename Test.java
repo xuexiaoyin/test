3ff145711dc9e03a9d995bc1217566f5fdff079b
xuexiaoyin <50687698+xuexiaoyin@users.noreply.github.com>
2019-06-05 22:53:18 +0800

    Update Test.java
    T添加一个新的方法测试

--- Test.java
+++ Test.java
@@ -2,4 +2,7 @@ class Test{
 	public static void main(String[] args){
 		System.out.println("test.java调教");
 	}
+      public static void test(){
+      System.out.print("testCESHIFANGFA");
+      }
 }
